468d2d43e71080ca897b41762f146b7e0258cc19
MGitea <93429689+MGitea@users.noreply.github.com>
2021-10-30 18:46:27 +0800

    Create Radamsa.cs

---
+++ Utility/Radamsa.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peach.Pro.Core.Mutators.Utility
+{
+    public class Radamsa
+    {
+        public static string FileName = "D:\\protocol-fuzzer-ce-main\\radamsa\\radamsa.exe";
+        public static string Arguments = "D:\\protocol-fuzzer-ce-main\\radamsa\\out.txt";
+        public class Blob
+        {
+            public static string Arguments = "D:\\protocol-fuzzer-ce-main\\radamsa\\out.txt -o D:\\protocol-fuzzer-ce-main\\radamsa\\tmp.txt";
+            public static string TmpFile = "D:\\protocol-fuzzer-ce-main\\radamsa\\tmp.txt";
+        }
+        
+    }
+}
